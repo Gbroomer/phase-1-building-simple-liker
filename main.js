2cede06750c33274d200fed09c6b02c51fd89b0d
Grant Bruemmer <grantbruemmer@gmail.com>
2023-06-16 12:41:17 -0500

    completed simple liker

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+let err = document.getElementById("modal")
+let errMessage = document.getElementById("modal-message")
 // Your JavaScript code goes here!
-
+document.addEventListener("click", function(e){
+  mimicServerCall()
+    .then(res => {
+      let eventTar = e.target
+      if (eventTar.className === "like-glyph") {
+        eventTar.className = "activated-heart"
+        eventTar.textContent = FULL_HEART
+      } else if (eventTar.className === "activated-heart") {
+        eventTar.className = "like-glyph"
+        eventTar.textContent = EMPTY_HEART
+      }
+    })
+    .catch(() => {
+        if (err.className === "hidden") {
+          err.className = ""
+          errMessage.textContent = "Error Loading Server"
+          setTimeout(() => {
+            err.className = "hidden"
+            errMessage.textContent = ""
+          }, 3000);
+        }
+    })
+})
 
 
 
